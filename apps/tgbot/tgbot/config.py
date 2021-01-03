4f0b36656c87c4011e21cf73b858893f51709667
kiriharu <kiriharu@yandex.ru>
2021-01-03 15:19:06 +0300

    moved project to package, format pep8, removed unused dependency

---
+++ apps/tgbot/tgbot/config.py
@@ -0,0 +1,4 @@
+import os
+
+# Loading token from .env
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
--- apps/tgbot/tgbot/handlers/default/__init__.py
+++ apps/tgbot/tgbot/handlers/default/__init__.py
@@ -2,5 +2,6 @@ from aiogram import Dispatcher
 
 from .start import start_cmd
 
+
 def setup(dp: Dispatcher):
     dp.register_message_handler(start_cmd, commands=['start'])
