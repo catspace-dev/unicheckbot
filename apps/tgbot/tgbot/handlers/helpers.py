9ca48f5973dac477098a9ea5acaa6e42f0307587
kiriharu <kiriharu@yandex.ru>
2021-01-05 00:33:55 +0300

    result Response(500) if node unreachable

--- apps/tgbot/tgbot/handlers/helpers.py
+++ apps/tgbot/tgbot/handlers/helpers.py
@@ -1,4 +1,4 @@
-from httpx import AsyncClient, Timeout
+from httpx import AsyncClient, Timeout, Response, ConnectError
 from typing import List
 from core.coretypes import APINode
 
@@ -15,8 +15,12 @@ def check_int(value) -> bool:
 async def send_api_requests(endpoint: str, data: dict, nodes: List[APINode]):
     for node in nodes:
         data.update(dict(token=node.token))
-        async with AsyncClient(timeout=Timeout(timeout=100.0)) as client:
-            result = await client.get(
-                f"{node.address}/{endpoint}", params=data
-            )
+        try:
+            async with AsyncClient(timeout=Timeout(timeout=100.0)) as client:
+                result = await client.get(
+                    f"{node.address}/{endpoint}", params=data
+                )
+        except ConnectError:
+            # We yield 500 response when backend is offline
+            result = Response(500)
         yield result
