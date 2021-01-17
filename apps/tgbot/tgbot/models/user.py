1bd0b6e78e2f4b431b415b4a942607d582b57bdb
kiriharu <kiriharu@yandex.ru>
2021-01-17 18:08:17 +0300

    added tortoise as dependency, for settings update

---
+++ apps/tgbot/tgbot/models/user.py
@@ -0,0 +1,7 @@
+from tortoise import fields, Model
+
+
+class User(Model):
+    telegram_id = fields.IntField(pk=True)
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
