7147d3c1ef9465bfa75604fc244f22ef50f39f29
kiriharu <kiriharu@yandex.ru>
2021-01-30 01:32:49 +0300

    aliases in dispatcher, new /start message

--- apps/tgbot/tgbot/handlers/default/__init__.py
+++ apps/tgbot/tgbot/handlers/default/__init__.py
@@ -10,10 +10,10 @@ from .ipcalc import IPCalcCommandHandler
 
 
 def setup(dp: Dispatcher):
-    dp.register_message_handler(start_cmd, is_forwarded=False, commands=['start'])
+    dp.register_message_handler(start_cmd, is_forwarded=False, commands=['start', 'help'])
     dp.register_message_handler(WebCheckerHandler().handler, is_forwarded=False, commands=['web', 'http'])
     dp.register_message_handler(WhoisCommandHandler().handler, is_forwarded=False, commands=['whois'])
     dp.register_message_handler(ICMPCheckerHandler().handler, is_forwarded=False, commands=['icmp', 'ping'])
     dp.register_message_handler(TCPCheckerHandler().handler, is_forwarded=False, commands=['tcp'])
-    dp.register_message_handler(MinecraftCheckerHandler().handler, is_forwarded=False, commands=['minecraft'])
+    dp.register_message_handler(MinecraftCheckerHandler().handler, is_forwarded=False, commands=['minecraft', 'mc'])
     dp.register_message_handler(IPCalcCommandHandler().handler, is_forwarded=False, commands=['ipcalc'])
